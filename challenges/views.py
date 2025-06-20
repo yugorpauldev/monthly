62db29663a12d8ab1e941fa81f0ec0336e8cf8e3
yugorpaul <nhantumboyugor@gmail.com>
2025-06-20 21:27:31 +0200

    Rendering html

--- challenges/views.py
+++ challenges/views.py
@@ -36,8 +36,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1> {challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render (request,"challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>This month is not supported</h1>") 
         
--- monthly/settings.py
+++ monthly/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'monthly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+           # BASE_DIR / "challenges" / "templates"
+            
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
